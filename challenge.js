4a5117979a6738123279175555c177dddff82d75
Natalia Banz <banznatalia@gmail.com>
2021-04-05 13:24:45 +0200

    challenges 1,2,3

---
+++ challenge.js
@@ -0,0 +1,105 @@
+//CHALLENGE 1
+/*
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 95;
+const johnHeight = 1.88;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const markHeigerBmi = markBMI > johnBMI;
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHeigerBmi);
+
+
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+const markHeigerBmi2 = markBMI2 > johnBMI2;
+
+console.log(markBMI2);
+console.log(johnBMI2);
+console.log(markHeigerBmi2);
+
+Other solutions
+console.log(markBMI2, johnBMI2, markHeigerBmi2);
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 95;
+const johnHeight = 1.88;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const markHeigerBmi = markBMI > johnBMI;
+
+console.log(markBMI2, johnBMI2, markHeigerBmi2);
+
+then comment out the old variables and put new dates
+// 
+
+// CHALLENGE 2
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 95;
+const johnHeight = 1.88;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(markBMI, johnBMI);
+
+if (markBMI > johnBMI) {
+	console.log(`Mark's BMI (${markBMI}) is higher than John's!`);
+} else {
+	console.log(`John's BMI (${johnBMI}) is higher than Mark's!`);
+}
+
+
+// CHALLENGE 3
+
+const dolphFirst = 97;
+const dolphSecond = 112;
+const dolphThird = 101;
+
+const koalasFirst = 109;
+const koalasSecond = 95;
+const koalasThird = 106;
+
+const dolphinsAv = (dolphFirst + dolphSecond + dolphThird) / 3;
+console.log(dolphinsAv);
+const koalasAv = (koalasFirst + koalasSecond + koalasThird) / 3;
+console.log(koalasAv);
+ 1
+if (dolphinsAv > koalasAv) {
+	console.log("Dolphins won!");
+} else if (dolphinsAv === koalasAv) {
+	console.log("Remis!!");
+} else {
+	console.log("Koalas won");
+}
+
+2
+if (dolphinsAv > koalasAv && dolphinsAv >= 100) {
+	console.log("Dophins win!");
+} else if (koalasAv > dolphinsAv && koalasAv >= 100) {
+	console.log("Koalas win");
+} else {
+	console.log("we dont have winner");
+}
+
+
+if (dolphinsAv > koalasAv && dolphinsAv >= 100) {
+	console.log("Dophins win!");
+} else if (koalasAv > dolphinsAv && koalasAv >= 100) {
+	console.log("Koalas win");
+} else if (dolphinsAv === koalasAv && dolphinsAv >= 100 && koalasAv >= 100) {
+	console.log("Both team won!");
+}
+End of bonuses
+*/
