7d1f51cbac380e3ba64858eef79db65ba7868db2
Natalia Banz <banznatalia@gmail.com>
2021-04-05 14:36:13 +0200

    challange 4

--- challenge.js
+++ challenge.js
@@ -101,5 +101,21 @@ if (dolphinsAv > koalasAv && dolphinsAv >= 100) {
 } else if (dolphinsAv === koalasAv && dolphinsAv >= 100 && koalasAv >= 100) {
 	console.log("Both team won!");
 }
-End of bonuses
+
 */
+
+// CHALLANGE 4
+
+const bill = 430;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(tip);
+
+const billTip = bill + tip;
+console.log(billTip);
+
+console.log(
+	`The bill was ${bill}, and the tip was ${tip}, final amount to pay was ${
+		bill + tip
+	}`
+);
