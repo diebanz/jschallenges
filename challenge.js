7a0196ba95263355ecad410a32f448f73a71f985
Natalia Banz <banznatalia@gmail.com>
2021-04-06 16:50:40 +0200

    challenge function

--- challenge.js
+++ challenge.js
@@ -102,9 +102,9 @@ if (dolphinsAv > koalasAv && dolphinsAv >= 100) {
 	console.log("Both team won!");
 }
 
-*/
 
-// CHALLANGE 4
+
+// CHALLENGE 4
 
 const bill = 430;
 
@@ -119,3 +119,32 @@ console.log(
 		bill + tip
 	}`
 );
+*/
+
+// CHALLENGE 5
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// TEST 1
+let scoreDolphin = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgD, avgK) {
+	if (avgD >= 2 * avgK) {
+		console.log(`Dolp wins (${avgD} vs. ${avgK})`);
+	} else if (avgK >= 2 * avgD) {
+		console.log(`Koala wins (${avgK} vs. ${avgD})`);
+	} else {
+		console.log("No one won");
+	}
+};
+
+console.log(checkWinner(scoreKoalas, scoreDolphin));
+
+// checkWinner(555, 1200);
+
+// Test 2
+scoreDolphin = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphin, scoreKoalas);
+checkWinner(scoreDolphin, scoreKoalas);
