bca3ea351beafd28823cc8fe3094b11cdcfb7d26
Natalia Banz <banznatalia@gmail.com>
2021-07-26 13:36:09 +0200

    4th challenge

--- 6.js
+++ 6.js
@@ -1,13 +1,14 @@
-// CHALLENGE 6
-
-const bill = 430;
-
-const calcTip = function (bill) {
-	return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[1] + tips[2]];
-console.log(bills, tips, totals);
+// Developer Skills & Editor Setup
+// Coding Challenge #1
+// Given an array of forecasted maximum temperatures, the thermometer displays a
+// string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+// days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+// string like the above to the console. Try it with both test datasets.
+// 2. Use the problem-solving framework: Understand the problem and break it up
+// into sub-problems!
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data 2: [12, 5, -5, 0, 4]
+// // GOOD LUCK 😀
